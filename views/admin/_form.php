ea26d90b3ff9d77a5cf652e50e59acd7f7e5d5a7
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-03-15 18:26:12 +0700

    change offset to bootstrap3

--- views/admin/_form.php
+++ views/admin/_form.php
@@ -29,39 +29,39 @@ use ommu\testimony\models\TestimonyCategory;
 
 <?php 
 $user = isset($model->user) ? $model->user->displayname : '-';
-echo $form->field($model, 'user_id', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12 checkbox">'.$user.'</div>'])
+echo $form->field($model, 'user_id', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12 checkbox">'.$user.'</div>'])
 	->textInput(['type' => 'number', 'min' => '1'])
 	->label($model->getAttributeLabel('user_id'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']); ?>
 
 <?php 
 $member = isset($model->member) ? $model->member->member_id : '-';
-echo $form->field($model, 'member_id', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12 checkbox">'.$member.'</div>'])
+echo $form->field($model, 'member_id', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12 checkbox">'.$member.'</div>'])
 	->textInput(['type' => 'number', 'min' => '1'])
 	->label($model->getAttributeLabel('member_id'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']); ?>
 
 <?php 
 $cat_id = TestimonyCategory::getCategory(1);
-echo $form->field($model, 'cat_id', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
+echo $form->field($model, 'cat_id', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
 	->dropDownList($cat_id, ['prompt'=>''])
 	->label($model->getAttributeLabel('cat_id'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']); ?>
 
 <?php if($model->cat_id == null || (isset($model->category) && $model->category->rate_status == 1)):
-echo $form->field($model, 'testimony_rate', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
+echo $form->field($model, 'testimony_rate', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
 	->textInput(['type' => 'number', 'min' => '1', 'maxlength' => true])
 	->label($model->getAttributeLabel('testimony_rate'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']);
 endif; ?>
 
-<?php echo $form->field($model, 'testimony_message', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
+<?php echo $form->field($model, 'testimony_message', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
 	->textarea(['rows'=>6, 'cols'=>50])
 	->label($model->getAttributeLabel('testimony_message'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']); ?>
 
-<?php echo $form->field($model, 'publish', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12 checkbox">{input}{error}</div>'])
+<?php echo $form->field($model, 'publish', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12 checkbox">{input}{error}</div>'])
 	->checkbox(['label'=>''])
 	->label($model->getAttributeLabel('publish'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']); ?>
 
 <div class="ln_solid"></div>
 <div class="form-group">
-	<div class="col-md-9 col-sm-9 col-xs-12 col-12 col-md-offset-3">
+	<div class="col-md-6 col-sm-9 col-xs-12 col-12 col-sm-offset-3">
 		<?php echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']); ?>
 	</div>
 </div>
--- views/category/_form.php
+++ views/category/_form.php
@@ -26,25 +26,25 @@ use app\components\ActiveForm;
 
 <?php //echo $form->errorSummary($model);?>
 
-<?php echo $form->field($model, 'category_name_i', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
+<?php echo $form->field($model, 'category_name_i', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
 	->textInput(['maxlength' => true])
 	->label($model->getAttributeLabel('category_name_i'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']); ?>
 
-<?php echo $form->field($model, 'category_desc_i', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
+<?php echo $form->field($model, 'category_desc_i', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12">{input}{error}</div>'])
 	->textarea(['rows'=>6, 'cols'=>50, 'maxlength' => true])
 	->label($model->getAttributeLabel('category_desc_i'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']); ?>
 
-<?php echo $form->field($model, 'rate_status', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12 checkbox">{input}{error}</div>'])
+<?php echo $form->field($model, 'rate_status', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12 checkbox">{input}{error}</div>'])
 	->checkbox(['label'=>''])
 	->label($model->getAttributeLabel('rate_status'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']); ?>
 
-<?php echo $form->field($model, 'publish', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 col-12 checkbox">{input}{error}</div>'])
+<?php echo $form->field($model, 'publish', ['template' => '{label}<div class="col-md-6 col-sm-9 col-xs-12 col-12 checkbox">{input}{error}</div>'])
 	->checkbox(['label'=>''])
 	->label($model->getAttributeLabel('publish'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12 col-12']); ?>
 
 <div class="ln_solid"></div>
 <div class="form-group">
-	<div class="col-md-9 col-sm-9 col-xs-12 col-12 col-md-offset-3">
+	<div class="col-md-6 col-sm-9 col-xs-12 col-12 col-sm-offset-3">
 		<?php echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']); ?>
 	</div>
 </div>
