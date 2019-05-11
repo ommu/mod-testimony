9d28e47850a08afb722d641a886f86abb5027f0a
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-11 11:47:59 +0700

    commit

--- views/admin/_search.php
+++ views/admin/_search.php
@@ -48,8 +48,10 @@ use yii\widgets\ActiveForm;
 			->input('date');?>
 
 		<div class="form-group">
-			<?php echo Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-			<?php echo Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+			<?php echo Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']); ?>
+			<?php echo Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']); ?>
 		</div>
+
 	<?php ActiveForm::end(); ?>
+
 </div>
