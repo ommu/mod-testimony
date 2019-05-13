e1dc954cc4d4519f7c077c996d08a6ab3c469961
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-13 11:07:13 +0700

    commit

--- models/TestimonyCategory.php
+++ models/TestimonyCategory.php
@@ -159,7 +159,7 @@ class TestimonyCategory extends \app\components\ActiveRecord
 
 		$this->templateColumns['_no'] = [
 			'header' => Yii::t('app', 'No'),
-			'class'  => 'yii\grid\SerialColumn',
+			'class' => 'yii\grid\SerialColumn',
 			'contentOptions' => ['class'=>'center'],
 		];
 		$this->templateColumns['category_name_i'] = [
--- models/TestimonySetting.php
+++ models/TestimonySetting.php
@@ -99,7 +99,7 @@ class TestimonySetting extends \app\components\ActiveRecord
 
 		$this->templateColumns['_no'] = [
 			'header' => Yii::t('app', 'No'),
-			'class'  => 'yii\grid\SerialColumn',
+			'class' => 'yii\grid\SerialColumn',
 			'contentOptions' => ['class'=>'center'],
 		];
 		$this->templateColumns['license'] = [
