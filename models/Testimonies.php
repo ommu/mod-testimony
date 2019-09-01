ec6e39d4631066b7153c43c5306c53dca02397bd
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-09-01 19:59:13 +0700

    change no with # in gridview

--- models/Testimonies.php
+++ models/Testimonies.php
@@ -152,7 +152,7 @@ class Testimonies extends \app\components\ActiveRecord
 			return;
 
 		$this->templateColumns['_no'] = [
-			'header' => Yii::t('app', 'No'),
+			'header' => '#',
 			'class' => 'yii\grid\SerialColumn',
 			'contentOptions' => ['class'=>'center'],
 		];
--- models/TestimonyCategory.php
+++ models/TestimonyCategory.php
@@ -161,7 +161,7 @@ class TestimonyCategory extends \app\components\ActiveRecord
 			return;
 
 		$this->templateColumns['_no'] = [
-			'header' => Yii::t('app', 'No'),
+			'header' => '#',
 			'class' => 'yii\grid\SerialColumn',
 			'contentOptions' => ['class'=>'center'],
 		];
