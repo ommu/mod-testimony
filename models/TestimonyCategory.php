6a300f0594cb4fae46c8162e7f306f4e7f899aea
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-10-22 17:55:49 +0700

    add condition jika model tidak dipanggil pada mode search (gridview or with dataProvider)

--- models/TestimonyCategory.php
+++ models/TestimonyCategory.php
@@ -160,6 +160,9 @@ class TestimonyCategory extends \app\components\ActiveRecord
 		if(!(Yii::$app instanceof \app\components\Application))
 			return;
 
+		if(!$this->hasMethod('search'))
+			return;
+
 		$this->templateColumns['_no'] = [
 			'header' => '#',
 			'class' => 'yii\grid\SerialColumn',
